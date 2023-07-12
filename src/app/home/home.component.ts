ee7757c7a5089150e37ff6edf96cd3d2f1a5a50a
igor <arqurb.igor@gmail.com>
2023-07-12 19:05:55 -0300

    add função edit e delete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,11 +12,33 @@ export class HomeComponent {
     { toDoTask: 'wash clothes', done: false },
   ];
 
+  chosenIndex: number = 0;
+
   onSubmit(form: NgForm) {
     this.tasks.push({ toDoTask: form.controls['toDoTask'].value, done: false });
 
     console.log(this.tasks);
 
+    form.reset();
+  }
+
+  onCheck(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+    console.log(this.tasks);
+  }
+
+  setIndex(index: number) {
+    this.chosenIndex = index;
+  }
+
+  onDelete(index: number) {
+    this.tasks.splice(index, 1);
+    console.log(this.tasks);
+  }
+
+  onEdit(form: NgForm, index: number) {
+    console.log(form, index);
+    this.tasks[index].toDoTask = form.controls['toDoTask'].value;
     form.reset();
   }
 }
